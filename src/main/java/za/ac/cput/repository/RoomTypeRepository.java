526d0a099be5fdf471c16b662807b44abb494450
Asteroth72Pillars <75275630+Asteroth72Pillars@users.noreply.github.com>
2024-05-17 22:37:35 +0200

    Add files via upload
    Updated repository classes

---
+++ src/main/java/za/ac/cput/repository/RoomTypeRepository.java
@@ -0,0 +1,13 @@
+/*
+RoomTypeRepository.java
+RoomTypeRepository interface
+Author: Moegamat Zaihd Behardien (219233829)
+Date: 23 March 2024
+*/
+
+package za.ac.cput.repository;
+import za.ac.cput.domain.RoomType;
+public interface RoomTypeRepository extends IRepository<RoomType, Long>
+{
+
+}
