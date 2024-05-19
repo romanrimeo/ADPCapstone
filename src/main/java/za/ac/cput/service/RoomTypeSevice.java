ff4db9173893fa86744304c8b84bc549d1283571
Asteroth72Pillars <219233829@mycput.ac.za>
2024-05-19 20:24:19 +0200

    Updated

---
+++ src/main/java/za/ac/cput/service/RoomTypeSevice.java
@@ -0,0 +1,38 @@
+package za.ac.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import za.ac.cput.domain.RoomType;
+import za.ac.cput.repository.RoomTypeRepository;
+
+import java.util.List;
+
+public class RoomTypeSevice
+{
+    private RoomTypeRepository repository;
+    @Autowired
+    RoomTypeSevice(RoomTypeRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    @Override
+    public RoomType create(RoomType roomType)
+    {
+        return repository.create(roomType);
+    }
+
+    @Override
+    public RoomType read(Id id)
+    {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<RoomType> getAll()
+    {
+        return repository.findAll();
+    }
+
+
+
+}
---
+++ src/main/java/za/ac/cput/service/iRoomTypeService.java
@@ -0,0 +1,10 @@
+package za.ac.cput.service;
+
+import za.ac.cput.domain.RoomType;
+
+import java.util.List;
+
+public interface iRoomTypeService
+{
+    List<RoomType> getAll();
+}
